b91f7b9090bc3222da73b3f04c13f393fe81e5f9
xiaobin liu <327847390@qq.com>
2019-01-11 18:10:30 +0800

    -added: AVCaptureSession

---
+++ XBAVFoundation/Classes/AVCaptureSession/XBCameraController.swift
@@ -0,0 +1,9 @@
+//
+//  XBCameraController.swift
+//  XBAVFoundation
+//
+//  Created by xiaobin liu on 2019/1/11.
+//  Copyright © 2019 Sky. All rights reserved.
+//
+
+import Foundation
---
+++ XBAVFoundation/Classes/AVCaptureSession/XBCaptureSessionC.swift
@@ -0,0 +1,30 @@
+//
+//  XBCaptureSessionC.swift
+//  XBAVFoundation
+//
+//  Created by xiaobin liu on 2019/1/11.
+//  Copyright © 2019 Sky. All rights reserved.
+//
+
+import UIKit
+
+class XBCaptureSessionC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
